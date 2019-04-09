049f4bc8ca49032b9d2819b6775712166949f966
heitorrbarros <heitorrbarros@gmail.com>
2019-04-08 23:45:10 -0300

    algumas coisas

--- src/string-victorize.ts
+++ src/string-victorize.ts
@@ -1,5 +1,17 @@
+import { padStart } from 'lodash';
 export class StringVictorizer {
-    static victorize(value: String): String {
-        return value.replace(/ /g, '  ');
+    static victorize(value: String,
+        opts: VictorizationOptions =
+            { repeatValue: ' ', numberOfRepeats: 2 }): String {
+        let repeatValue = opts.repeatValue || ' ';
+        repeatValue = padStart(repeatValue, opts.numberOfRepeats);
+
+        return value.replace(/ /g, repeatValue);
     }
+}
+
+interface VictorizationOptions {
+    repeatValue: String,
+    numberOfRepeats: number,
+    toUpper?: Boolean
 }
--- test/string-victorize.test.ts
+++ test/string-victorize.test.ts
@@ -5,3 +5,9 @@ test('Deve vicquitorizar uma string', () => {
     const victorized = 'Essa  string  deve  ser  vicquitorizada';
     expect(out).toBe(victorized);
 });
+
+test('Deve substituir espaços por caracter selecionado', () => {
+    const out = StringVictorizer.victorize('Essa eh outra Papai', { repeatChar: '*', numberOfRepeats: 3 });
+    const victorized = 'Essa***eh***Outra***Papai';
+    expect(out).toBe(victorized);
+});
